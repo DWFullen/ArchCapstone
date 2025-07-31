50c1d826b7525bf23521a4ac40fb6556125e2d5c
DWFullen <douglas.fullen@gmail.com>
2025-07-31 17:42:39 -0400

    Refactor code structure for improved readability and maintainability

--- MyBlazorApp/obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
+++ MyBlazorApp/obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19d7f5ecddd55856c3035338f63f0121072f6246")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e49fc466a5c33faaf403acb788cf49dbf914c7c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- docs/btcDocs/btcPayServerReferences.md
+++ docs/btcDocs/btcPayServerReferences.md
@@ -4,5 +4,8 @@ https://btcpayserver.org/
 # Deployment Options
 https://docs.btcpayserver.org/Deployment/
 
+# API Guide
+https://docs.btcpayserver.org/API/Greenfield/v1/#tag/Notifications-(Current-User)
+
 # Github
 https://github.com/btcpayserver
